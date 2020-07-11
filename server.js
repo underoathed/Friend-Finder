604169f91ee9304e5737ae82a2725b9935948f2a
Nick <nicholasroman86@gmail.com>
2020-07-11 14:06:52 -0400

    Added code for server.js and added in data into friends.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+
+
+var app = express()
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
